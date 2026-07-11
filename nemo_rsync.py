9a87433d63d0efb3013fa56f43755c836941e3d6
Gabriel <gabriel@localhost>
2026-07-11 21:50:26 +0300

    Updated nemo_rsync.py with adaptive location, graceful exit if no files are provided

--- nemo_rsync.py
+++ nemo_rsync.py
@@ -2,6 +2,7 @@
 
 import gi
 import sys
+import os
 
 from terminal_launcher import launch_terminal
 
@@ -9,6 +10,21 @@ from terminal_launcher import launch_terminal
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
+
+# import location of whereever the rsync runner is actually located if the folder location changes and constants 
+# determine the directory where this script is located
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# build the absolute path to the rsync_runner.sh
+RUNNER_SCRIPT = os.path.join(
+    SCRIPT_DIR,
+    "rsync_runner.sh"
+)
+
+# gracefully exit if no files are supplied
+if len(sys.argv) < 2:
+    print("No source files were supplied.")
+    sys.exit(1)
 
 # Create a folder selection dialog.
 dialog = Gtk.FileChooserDialog(
@@ -32,10 +48,11 @@ if response == Gtk.ResponseType.OK:
     destination = dialog.get_filename()
 
     # get source files except the first item because python program is always the first item
+    # Skip argv[0] (the Python script itself).
     sources = sys.argv[1:]
 
     launch_terminal(
-        "./rsync_runner.sh",
+        RUNNER_SCRIPT,
         sources,
         destination
     )
